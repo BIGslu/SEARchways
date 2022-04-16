44d8090b509c84d26352d116c90aa6daaf2d3299
Kim Dill-McFarland <kdillmcfarland@gmail.com>
2022-04-15 19:02:49 -0700

    BIGsea update
    - clean names to match other fxns in BIGverse
    - allow Broad gene set list (msigdbr) or custom (db)
    - allow HGNC symbol, ENSEMBL, or ENTREZ gene ID
    - format output to match other fxns in BIGverse

--- R/BIGsea.R
+++ R/BIGsea.R
@@ -1,12 +1,13 @@
 #' @title BIGsea
-#' @description Run GSEA on multiple list using fgsea. More information about GSEA and geneset databases check https://www.gsea-msigdb.org/gsea/msigdb
-#' @param gene_list named list object with named numeric vectors of gene symbols and logFC
-#' @param gmt_ls list object with gene ontology data
-#' @param nperm number of permutations for P-value calculations. Default is 1000
-#' @param group Which Geneset database you want to run GSEA on
-#' @param subgroup Specify subgroup of Geneset database you want to run GSEA on. Below is the list of commonly used Genesets \cr
+#' @description Run gene set enrichment analysis (GSEA) on multiple list using fgsea. More information about GSEA and geneset databases check https://www.gsea-msigdb.org/gsea/msigdb
+#' @param gene_list Named list object with named numeric vectors of gene symbols and logFC
+#' @param ID Character string for type of ID used in gene_list. One of SYMBOL, ENTREZ, ENSEMBL. Default is SYMBOL
+#' @param nperm Numeric permutations for P-value calculations. Default is 1000
+#' @param species Character string denoting species of interest. Default is human
+#' @param category Character string denoting Broad gene set database
+#' @param subcategory Character string denoting Broad gene set sub-database \cr
 #' \tabular{rrrrr}{
-#'  \strong{Group} \tab    \strong{Sub-group}\cr
+#'  \strong{category} \tab    \strong{subcategory}\cr
 #'  C1  \tab      \cr
 #'  C2  \tab      CGP\cr
 #'  C2  \tab      CP\cr
@@ -23,31 +24,60 @@
 #'  C6  \tab      \cr
 #'  H   \tab      \cr
 #'  }
-#' @param S Specify specie for Geneset database default is human
+#' @param db If not using Broad databases, a data frame with gene ontology including category (gs_cat), subcategory (gs_subcat), gene set name (gs_name), and gene ID (gene_symbol, entrez_gene, or ensembl_gene as matches your gene_list names)
 #'
 #' @return Output of a list of list with GSEA results
 #' @export
 #'
 #' @examples
-BIGsea <- function(gene_list, gmt_ls=NULL, nperm=1000,group = NULL, subgroup = NULL, S = "human"){
-  pathway <- pval <- padj <- ES <- NES <- size <- leadingEdge <- fgsea.FC  <- NULL
+#' BIGsea(example_gene_list, category="H", ID="ENSEMBL")
+#' BIGsea(example_gene_list, category="C2", subcategory="CP", ID="ENSEMBL")
+
+BIGsea <- function(gene_list, nperm=1000, species="human", ID="SYMBOL",
+                   category = NULL, subcategory = NULL, db = NULL){
+  ensembl_gene <- entrez_gene <- gene_symbol <- group <- gs_name <- gs_subcat <- padj <- pathway <- NULL
   #Blank list to hold results
   all.results <- list()
 
   #### Data ####
   #Load gene ontology
-  if(!is.null(group) & !is.null(subgroup)){
-
-    gene_sets = msigdbr::msigdbr(species = S, category = group, subcategory = subgroup)
-    myGO <- split(x = gene_sets$gene_symbol, f = gene_sets$gs_name)
-  } else if(!is.null(group) & is.null(subgroup)){
-    gene_sets = msigdbr::msigdbr(species = S, category = group)
-    myGO <- split(x = gene_sets$gene_symbol, f = gene_sets$gs_name)
-  }else if(!is.null(gmt_ls)){
-    myGO <- gmt_ls
+  if(!is.null(category)){
+    db.format <- msigdbr::msigdbr(species, category)
+    #Subset subcategory if selected
+    if(!is.null(subcategory)){
+      db.format <- db.format %>%
+        dplyr::filter(grepl(paste0("^",subcategory), gs_subcat))
+    }
+  } else if(!is.null(db)){
+    db.format <- db
   } else {
-    stop("Please provide gene set data as file for list object.")
+    stop("Please provide gene set information as Broad category/subcategory or in a data frame as db.")
   }
+
+  #Get gene ID
+  if(ID == "SYMBOL"){
+    db.format2 <- db.format %>%
+      dplyr::group_by(gs_name) %>%
+      dplyr::summarise(genes = list(gene_symbol)) %>%
+      dplyr::ungroup() %>%  tibble::column_to_rownames("gs_name")
+  } else if(ID == "ENSEMBL"){
+    db.format2 <- db.format %>%
+      dplyr::group_by(gs_name) %>%
+      dplyr::summarise(genes = list(ensembl_gene)) %>%
+      dplyr::ungroup() %>%  tibble::column_to_rownames("gs_name")
+  } else if(ID == "ENTREZ"){
+    db.format2 <- db.format %>%
+      dplyr::group_by(gs_name) %>%
+      dplyr::summarise(genes = list(entrez_gene)) %>%
+      dplyr::ungroup() %>%  tibble::column_to_rownames("gs_name")
+  } else{
+    stop("Please like ID from SYMBOL, ENSEMBL, or ENTREZ.")
+  }
+
+  #Format database to list
+  db.ls <- lapply(as.list(1:dim(db.format2)[1]),
+                  function(x) unlist(db.format2[x[1],]))
+  names(db.ls) <- rownames(db.format2)
 
   #### Loop ####
   #Loop through each list in the gene_list object
@@ -71,45 +101,26 @@ BIGsea <- function(gene_list, gmt_ls=NULL, nperm=1000,group = NULL, subgroup = N
     }
 
     #Run GSEA with fgsea
-    fg.result <- fgsea::fgseaSimple(pathways = myGO,
+    fg.result <- fgsea::fgseaSimple(pathways = db.ls,
                                     stats = genes.temp,
                                     nperm=nperm,
                                     #eps=0,
                                     scoreType=scoreType) %>%
-      as.data.frame()
-
-
-
-
-    #### Combine results ####
-    gsea.result <- fg.result %>%
-      dplyr::select(pathway, pval, padj, ES, NES, size, leadingEdge) %>%
-      dplyr::mutate(fgsea.FC = ifelse(NES < 0, "down","up")) %>%
-      #format leading edge list
-      tidyr::unnest(cols = c(leadingEdge)) %>%
-      dplyr::group_by(pathway,pval,padj,ES,
-               NES,size,fgsea.FC) %>%
-      dplyr::summarise(leadingEdge=paste(unique(leadingEdge), collapse=";"),
-                       .groups="drop")
+      as.data.frame() %>%
+      dplyr::rename(FDR=padj) %>%
+      dplyr::mutate(gs_cat=category, gs_subcat=subcategory, .before=1)
 
     #### Save ####
-    all.results[[genes]] <- gsea.result
+    all.results[[genes]] <- fg.result
   }
 
   #### Format output ####
   #Unlist results into 1 df
-
-
-
-  if(group!= "c6_GSEA.result"){
-    all.results.df <- do.call(rbind.data.frame, all.results) %>%
-      tibble::rownames_to_column("group") %>%
-      dplyr::mutate(group = gsub("[.][0-9]{0,4}","",group)) %>% dplyr::mutate(pathway = sub("[A-Z]*_","",pathway)) %>% dplyr::mutate(pathway = gsub("_"," ",pathway))
-  } else {
-    all.results.df <- do.call(rbind.data.frame, all.results) %>%
-      tibble::rownames_to_column("group") %>%
-      dplyr::mutate(group = gsub("[.][0-9]{0,4}","",group))
-  }
+  all.results.df <- do.call(rbind.data.frame, all.results) %>%
+    tibble::rownames_to_column("group") %>%
+    dplyr::mutate(group = gsub("[.][0-9]{0,4}","",group)) %>%
+    dplyr::mutate(pathway = sub("[A-Z]*_","",pathway)) %>%
+    dplyr::mutate(pathway = gsub("_"," ",pathway))
 
   return(all.results.df)
 }

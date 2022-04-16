a85e80b20cbd95af778dcf0dadbc1b6f028d1c4b
Kim Dill-McFarland <kdillmcfarland@gmail.com>
2022-04-15 18:59:27 -0700

    Add example data

---
+++ R/example_gene_list.R
@@ -0,0 +1,15 @@
+#' Example gene log2 fold change (FC) list
+#'
+#' @format A list of 2 vectors with 100 elements.
+#' \describe{
+#'   \item{HRV1}{numeric. Log2 FC of 100 random genes. Names are ENSEMBL ID.}
+#'   \item{HRV2}{numeric. Log2 FC of 100 random genes. Names are ENSEMBL ID.}
+#'       }
+#'
+#' @source \url{https://github.com/altman-lab/P259_pDC_public}
+#' @references Dill-McFarland et al. 2022. Eosinophil-mediated suppression and Anti-IL-5 enhancement of plasmacytoid dendritic cell interferon responses in asthma. J Allergy Clin Immunol. S0091-6749(22)00447-X. doi: 10.1016/j.jaci.2022.03.025. Online ahead of print.
+#' @description A list of names vectors, each containing numeric log2 fold change and gene ENSEMBL ID names.
+#' @docType data
+#' @name example_gene_list
+#' @keywords datasets
+"example_gene_list"
---
+++ data-raw/DATASET.R
@@ -0,0 +1,35 @@
+#### Example gene fold change list ####
+library(kimma)
+library(tidyverse)
+
+# Example expression data from kimma
+example.voom <- example.voom
+
+# Calculate mean fold change
+FC <- as.data.frame(example.voom$E) %>%
+  rownames_to_column() %>%
+  pivot_longer(-rowname, names_to = "libID") %>%
+  inner_join(example.voom$targets) %>%
+  select(rowname, ptID, virus, value) %>%
+  pivot_wider(names_from = virus) %>%
+  mutate(FC = HRV-none) %>%
+  group_by(rowname) %>%
+  summarise(meanFC=mean(FC, na.rm = TRUE), .groups="drop")
+
+# select 2 sets of 100 genes
+# Format as list
+set.seed(1879)
+HRV1 <- FC %>%
+  slice_sample(n=100) %>%
+  pull(meanFC, name=rowname)
+
+HRV2 <- FC %>%
+  slice_sample(n=100) %>%
+  pull(meanFC, name=rowname)
+
+example_gene_list <- list(
+  "HRV1" = HRV1,
+  "HRV2" = HRV2
+)
+
+usethis::use_data(example_gene_list, overwrite = TRUE)
